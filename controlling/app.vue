00eb0a97ff977385f3fe7d474c0e3051c8c56f14
Bela <122638570+belagrf@users.noreply.github.com>
2024-10-09 12:42:50 +0200

    create nuxt with pages

---
+++ controlling/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLayout />
+    <div class="bg-gray-100">
+    <NuxtPage />
+    </div>
+  </div>
+</template>
---
+++ controlling/pages/collection.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-4 w-[512px] h-screen m-auto flex flex-col items-center">
+        <p class="text-center font">Hier findest du alle Betriebswirtschaftlichen Auswertungen und kannst neue hinzufügen.</p>
+      <UButton class="mt-4 hover:bg-gray-900" size="md" color="black" icon="i-heroicons-rectangle-stack" label="BWAs hinzufügen" />
+      <UButton class="mt-4 hover:bg-gray-900" size="md" color="black" icon="i-heroicons-eye" label="Alle BWAs anzeigen" />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
